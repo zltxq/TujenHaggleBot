f8fe9a441f879ab775e81afdb5bc6b66f614a43a
zltxq <zltxxxx@gmail.com>
2023-08-05 00:11:30 +0300

    added loading names of templates + optimized buttons algorithm

--- main.py
+++ main.py
@@ -9,33 +9,26 @@ import sys
 # Function to load images from directory
 def load_images_from_directory(directory):
     image_list = []
+    image_names = []
     for entry in os.scandir(directory):
         if entry.name.endswith(".jpg"):
             img = cv2.imread(os.path.join(directory, entry.name), cv2.IMREAD_GRAYSCALE)
-            image_list.append((img))  # Store both name and image as a tuple
-    return image_list
-
-
-left_start = 300
-top_start = 345
+            image_list.append(img)
+            image_names.append(entry.name)
+    return image_list, image_names
 
 # Capture the screen within the specified region
 def capture_screen(screen_width = 680, screen_height = 650):
-    screen_region = (300, 345, screen_width, screen_height)
+    screen_region = (left_start, top_start, screen_width, screen_height)
     screenshot = pyautogui.screenshot(region=screen_region)
     frame = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     return gray_frame
 
-# Function to perform template matching and return matched locations
-# def find_matched_regions(captured_image, template):
-#     gray_frame = captured_image
-#     result = cv2.matchTemplate(gray_frame, template, cv2.TM_CCOEFF_NORMED)
-#     locations = np.where(result >= 0.8)
-#     locations = list(zip(*locations[::-1]))
-#     return locations
-def find_matched_regions(frame, template):
-    result = cv2.matchTemplate(frame, template, cv2.TM_CCOEFF_NORMED)
+
+def template_match(template):
+    screen_img = capture_screen()
+    result = cv2.matchTemplate(screen_img, template, cv2.TM_CCOEFF_NORMED)
     _, max_val, _, max_loc = cv2.minMaxLoc(result)
     # print(max_val)
     if max_val > .8:
@@ -47,11 +40,17 @@ def find_matched_regions(frame, template):
 buttons_path = "images/templates/buttons"
 currency_path = "images/templates/currency"
 
-buttons_images = load_images_from_directory(buttons_path)
-currency_images = load_images_from_directory(currency_path)
+currency_images = load_images_from_directory(currency_path)[0]
+currency_names = load_images_from_directory(currency_path)[1]
+buttons_images = load_images_from_directory(buttons_path)[0]
+buttons_names = load_images_from_directory(buttons_path)[1]
 
 # Initialize bot state and flag to keep track of the bot's running state
 running = False
+
+# margin left/top
+left_start = 300
+top_start = 345
 
 # Function to start the bot
 def start_bot():
@@ -61,10 +60,9 @@ def start_bot():
     currency_index = 0
 
     while running:
-
         frame = capture_screen()
 
-        first_check = find_matched_regions(frame, currency_images[currency_index])
+        first_check = template_match(currency_images[currency_index])
         if first_check is not None:
             # pyautogui.moveTo(first_check[0] + left_start + 2, first_check[1] + top_start + 2)
 
@@ -73,11 +71,15 @@ def start_bot():
             top_left = first_check
             bottom_right = (top_left[0] + w, top_left[1] + h)
             cv2.rectangle(frame, top_left, bottom_right, (255, 255, 255), 2)
+
+            print(currency_names[currency_index])
+
         else:
             currency_index += 1
 
         if currency_index >= len(currency_images):
-            reroll_btn = find_matched_regions(frame, buttons_images[1])
+            index_btn = buttons_names.index('reroll.jpg')
+            reroll_btn = template_match(buttons_images[index_btn])
             if reroll_btn is not None:
                 pyautogui.moveTo(reroll_btn[0] + left_start + 12, reroll_btn[1] + top_start + 12, .2)
                 pyautogui.click()
